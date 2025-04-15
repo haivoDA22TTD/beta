e886b2916d492a3c226901f4910c333a48cb10ba
PDKhai2k4 <11.12c3phandinhkhaicvt@gmail.com>
2025-04-15 08:01:56 +0700

    script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("actionButton").addEventListener("click", function() {
+    const members = ["Võ Chí Hải", "Phan Đình Khải", "Đàm Thúy Hiền"];
+    alert("Các thành viên nhóm: " + members.join(", "));
+});
